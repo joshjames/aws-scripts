328252f1db08411735b77bbe232bc844b06193a2
Josh Saunders <josh.saunders@virginaustralia.com>
2017-12-21 23:43:37 +1000

    added add-my-ip script

---
+++ add-my-ip.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+SECURITYGROUPNAME="my-security-group"
+# script to pull my current public IP address 
+# and add a rule to my EC2 security group allowing me SSH access 
+curl v4.ifconfig.co > ip.txt
+awk '{ print $0 "/32" }' < ip.txt > ipnew.txt
+export stuff=$(cat ipnew.txt)
+aws ec2 authorize-security-group-ingress --group-name $SECURITYGROUPNAME \
+ --protocol tcp --port 443 --cidr $stuff

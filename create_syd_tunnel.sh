27dc2867d46bb1b3f7261741af95c25181b75721
joshjames <joshsaunders@gmail.com>
2018-05-15 00:19:36 +1000

    created tunnel script for syd and kill

--- create_syd_tunnel.sh
+++ create_syd_tunnel.sh
@@ -4,22 +4,22 @@
 # dependencies: awscli bash jq
 #get instance id from name
 
-name=caliproxy
+name=sydproxy
 echo "getting instance details for $name"
-instance_id=`aws ec2 describe-instances --region us-west-1 --filters Name=tag:Name,Values=$name | jq '.Reservations[0] .Instances[0] .InstanceId'`
+instance_id=`aws ec2 describe-instances --filters Name=tag:Name,Values=$name | jq '.Reservations[0] .Instances[0] .InstanceId'`
 instance_id=`echo $instance_id | sed -e 's/^"//' -e 's/"$//'`
 
 #get instance state
 echo "Instance found: $instance_id getting power state..."
 echo "....."
-instance_state=`aws ec2 describe-instances --region us-west-1 --instance-ids $instance_id --output text --query 'Reservations[*].Instances[*].State.Name'`
+instance_state=`aws ec2 describe-instances --instance-ids $instance_id --output text --query 'Reservations[*].Instances[*].State.Name'`
 echo "instance state: $instance_state"
 #if instance is stopped turn it on
 if [[ $instance_state="stopped" ]] ; then
 echo "instance is off powering on..."
-aws ec2 start-instances --region us-west-1 --instance-ids=$instance_id
+aws ec2 start-instances --instance-ids=$instance_id
 echo "starting $name instance"
-while state=$(aws ec2 describe-instances --region us-west-1 --instance-ids $instance_id --output text --query 'Reservations[*].Instances[*].State.Name'); test "$state" = "pending"; do
+while state=$(aws ec2 describe-instances --instance-ids $instance_id --output text --query 'Reservations[*].Instances[*].State.Name'); test "$state" = "pending"; do
   sleep 2; echo -n '.'
 done; echo " $state"
 echo $name started: details 
@@ -31,13 +31,13 @@ fi
 echo "getting public IP address assigned..."
 
 #instance is on get public IP
-caliIP=`aws ec2 describe-instances --region us-west-1 --instance-ids $instance_id --output text --query 'Reservations[*].Instances[*].PublicIpAddress'`
+sydIP=`aws ec2 describe-instances --instance-ids $instance_id --output text --query 'Reservations[*].Instances[*].PublicIpAddress'`
 echo "IP address aquired: $caliIP"
 echo "creating ssh tunnel..."
-ssh -i ~/jjs2-kp2-cali.pem -D 1338 -f -C -q -N ubuntu@$caliIP -p 22
+ssh -i ~/jjs2-kp2.pem -D 1337 -f -C -q -N ubuntu@$sydIP -p 22
 echo "ssh tunnel to california created serving proxy on port 1338"
 
 echo "testing untunneled public IP..."
 curl ifconfig.co
 echo "testing tunneled public IP..."
-curl --socks5-hostname 127.0.0.1:1338 ifconfig.co
+curl --socks5-hostname 127.0.0.1:1337 ifconfig.co
